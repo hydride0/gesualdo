a5e6fae14ef27b9da3e605f0522b57d80c8e291d
hydride0 <erkiwi@mail.com>
2013-09-25 23:41:48 +0200

    Update gesualdo.rb, now his life has kinda sense
    -Stupid phrases
    +Unified url titler
    +Random replies to questions
    +A simple poll system
    +A various set of italian proverbs
    +Improved swag.

--- gesualdo.rb
+++ gesualdo.rb
@@ -4,29 +4,93 @@ require 'cinch/plugins/login'
 require 'nokogiri'
 require 'open-uri'
 
-Cinch::Bot.new {
+bot = Cinch::Bot.new do
   configure do |c|
-   c.nick = 'Gesualdo'
-   c.realname = c.nick 
-   c.user = c.nick
-   c.password = 'PASSWORD'
-   c.plugins.plugins = [Cinch::Plugins::Login]
-   c.plugins.options[Cinch::Plugins::Login] = { :password => 'PASSWORD' }
-   c.server = "SERVER"
-   c.channels = ["#CHANNEL"]
+    c.nick = 'Gesualdo'
+    c.realname = c.nick
+    c.user = c.nick
+    c.password = 'PAXX'
+    c.plugins.plugins = [Cinch::Plugins::Login]
+    c.plugins.options[Cinch::Plugins::Login] = { :password => 'PAXX' }
+    c.server = "irc.niggazwithattitu.de"
+    c.channels = ["#japanfags,#nwa"]
+  end
+
+  @poll = nil
+  @poll_owner = ''
+  reply = [].tap { |ary| File.read('reply.txt').each_line { |line| ary << line } }
+  proverbio = [].tap { |ary| File.read('proverbi.txt').each_line { |line| ary << line } }
+
+  on :message, /http(s)?:\/\/(\S+)/ do |m, ssl, url|
+     page = Nokogiri::HTML(open("http#{ssl}://#{url}").read, nil, 'utf-8')
+     if url.include? ( 'youtube.com' || 'youtu.be' )
+      m.reply Format(:lime,  page.css('//title').first.text.chomp(' - YouTube'))
+     else
+       m.reply Format(:teal, page.css('//title').first.text)
+     end
+   end
+
+  on :message, /^!poll ([^;]+);([^\/]+)\/(.+)$/ do  |m, topic, opt_a, opt_b|
+    if @poll != nil
+      m.reply Format(:red, 'A poll is currently in progress')
+    elsif @poll == nil && (opt_a.downcase != opt_b.downcase)
+      m.reply "Poll '#{topic}' started by #{m.user.nick}, vote with '!pvote #{opt_a}' or '!pvote #{opt_b}'. Stop the poll with '!pstop'."
+      @poll_owner = m.user.nick
+      @poll = 1
+      @glob_a = opt_a
+      @gl_b = opt_b
+      @a_count = 0
+      @b_count = 0
+      @who_vote = []
+    else
+      m.reply 'Error: control the syntax and don\'t be an asshole.'
+    end
+  end
+
+  on :message, /^!pvote (\S+)$/ do |m, vote|
+    if vote == @glob_a && !@who_vote.include?(m.user.nick)
+      @a_count += 1
+      @who_vote <<  m.user.nick
+    elsif vote == @glob_b && !@who_vote.include?(m.user.nick)
+      @b_count += 1
+      @who_vote << m.user.nick
+    else
+      m.reply 'Invalid vote.'
+    end
+  end
+
+  on :message, /^!pstop$/ do |m|
+    partic = @a_count + @b_count
+    if @poll != nil && (m.user.nick == 'Hydride' || m.user.nick == @poll_owner)
+        m.reply 'Poll is ended'
+        m.reply "#{partic} user(s) voted, #{@a_count} voted '#{@glob_a}' and #{@b_count} voted '#{@glob_b}'"
+        @poll = nil
+    elsif m.user.nick != 'Hydride' || m.user.nick != @poll_owner
+        m.reply 'Command denied.'
+    else
+        m.reply 'No poll in progress'
+    end
+  end
+
+  on :message, /^!prov$/ do |m|
+    m.reply proverbio[rand(1..369)]
+  end
+
+  on :message, /^!phelp$/ do |m|
+    m.reply 'Use !poll ARG;ARG/ARG to start a poll, !pvote ARG to vote and !pstop to stop the poll.'
+  end
+
+  on :message, /^!qu1t$/ do |m|
+    if m.user.nick == 'Hydride'
+      bot.quit
+    else
+      m.reply 'GTFO && ESAD, bitch'
+    end
+  end
+
+  on :message, /Gesualdo.*?/ do |m|
+    m.reply reply[rand(1..25)]
+  end
 end
-
-   on :message, /^http(s)?\:\/\/(www)?\.youtube\.com\/(.*)$/ do |m, ssl, www, url|
-     page = Nokogiri::HTML(open("http://www.youtube.com/#{url}").read, nil, 'utf-8')
-     m.reply  Format(:green, page.css('//title').first.text.chomp(' - YouTube'))
-   end
- 
-   on :message, /.*Gesualdo.*/ do |m|
-     m.reply "Le porgo i miei saluti, messer #{m.user.nick}"
-   end
-
-   on :message, /.*ALL'ATTACCO!.*/ do |m|
-     m.reply "#{m.user.nick}, ALLA PUGNA!"
-   end
-}.start
+bot.start
 
